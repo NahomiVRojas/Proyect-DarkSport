b63bd74b28f08900db96297c5b054515a26ea061
NahomiVRojas <nahomivillanueva2@gmail.com>
2023-09-04 14:53:20 -0500

    Cambios className

--- src/Components/MembersLi/MembersLi.jsx
+++ src/Components/MembersLi/MembersLi.jsx
@@ -12,20 +12,20 @@ export default function MembersLi({ selectedTab }) {
             <div className={style.allMembers}>
               {selectedTab === 'BASKETBALL COACHES' && (
                 <div className={style.teamCoaches}>
-                  <div className={style.divDaniel}>
+                  <div className={style.divMember}>
                     <img src={CoachDaniel} alt="Daniel Gonzales" className={style.imgDaniel}/>
-                    <h1 className={style.nameDaniel}>DANIEL GONZALES</h1>
-                    <p className={style.pDaniel}>Coach Level III - Argentina, Former Coach in the Argentine League, Head Coach of Rayo AQP, Former Coach in the Mitre Association Argentina, Former Coach in Tierra del Fuego Argentina.</p>
+                    <h1 className={style.nameMember}>DANIEL GONZALES</h1>
+                    <p className={style.pMember}>Coach Level III - Argentina, Former Coach in the Argentine League, Head Coach of Rayo AQP, Former Coach in the Mitre Association Argentina, Former Coach in Tierra del Fuego Argentina.</p>
                   </div>
-                  <div className={style.divEssau}>
+                  <div className={style.divMember}>
                     <img src={CoachEssau} alt="Essau Ramos" className={style.imgEssau}/>
-                    <h1 className={style.nameEssau}>ESSAU RAMOS</h1>
-                    <p className={style.pEssau}> No description</p>
+                    <h1 className={style.nameMember}>ESSAU RAMOS</h1>
+                    <p className={style.pMember}> No description</p>
                   </div>
-                  <div className={style.divIrin}>
+                  <div className={style.divMember}>
                     <img src={CoachIrin} alt="Irin Stark" className={style.imgIrin}/>
-                    <h1 className={style.nameIrin}>IRIN STARK</h1>
-                    <p className={style.pIrin}> No description</p>
+                    <h1 className={style.nameMember}>IRIN STARK</h1>
+                    <p className={style.pMember}> No description</p>
                   </div>
                 </div>
               )}
@@ -33,25 +33,25 @@ export default function MembersLi({ selectedTab }) {
       
             {selectedTab === 'PSYCHOLOGISTS ON THE TEAM' && (
               <div className={style.teamPsycologist}>
-                <div className={style.divDaniel}>
+                <div className={style.divMember}>
                   <img src={GildoVillanueva} alt="Gildo Villanueva" className={style.imgGildo} />
-                  <h1 className={style.nameDaniel}>GILDO VILLANUEVA</h1>
-                  <p className={style.pIrin}> No description</p>
+                  <h1 className={style.nameMember}>GILDO VILLANUEVA</h1>
+                  <p className={style.pMember}> No description</p>
                 </div>
-                <div className={style.divDaniel}>
+                <div className={style.divMember}>
                   <img src={JavierRojas} alt="Javier Rojas" className={style.imgGildo}/>
-                  <h1 className={style.nameDaniel}>JAVIER ROJAS</h1>
-                  <p className={style.pIrin}> No description</p>
+                  <h1 className={style.nameMember}>JAVIER ROJAS</h1>
+                  <p className={style.pMember}> No description</p>
                 </div>
               </div>
             )}
       
             {selectedTab === 'PHYSIOTHERAPIST' && (
               <div className={style.teamTerapist}>
-                <div className={style.divDaniel}>
+                <div className={style.divMember}>
                 <img src={BrayhamPhysio} alt="Brayham Delgado" className={style.imgGildo}/>
-                <h1 className={style.nameDaniel}>BRAYHAM DELGADO</h1>
-                <p className={style.pIrin}> No description</p>
+                <h1 className={style.nameMember}>BRAYHAM DELGADO</h1>
+                <p className={style.pMember}> No description</p>
                 </div>
               </div>
             )}
